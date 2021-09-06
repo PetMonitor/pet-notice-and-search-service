d0bb6366cfca26cc7c22f8318792b497abea1106
Maria Eugenia Mariotti <eugeniamariotti93@gmail.com>
2021-09-05 22:06:59 -0300

    Added login endpoint

--- app.py
+++ app.py
@@ -3,14 +3,19 @@ from flask_restful import Api
 from src.main.resources.notice import Notices, UserNotices, UserNotice
 from src.main.resources.pet import UserPet, UserPets, SimilarPets
 from src.main.resources.user import User, Users
+from src.main.resources.login import UserLogin, UserLogout
 
 app = Flask(__name__)
+app.secret_key = 'topSecretKey'
 api = Api(app, prefix='/api/v0')
 
 
 # We define all the endpoints handled by this service
 api.add_resource(User, '/users/<string:userId>', methods=['GET', 'PUT', 'DELETE'])
 api.add_resource(Users, '/users', methods=['GET', 'POST'])
+
+api.add_resource(UserLogin, '/users/login', methods=['POST'])
+api.add_resource(UserLogout, '/users/logout/<string:userId>', methods=['POST'])
 
 api.add_resource(UserPet, '/users/<string:userId>/pets/<string:petId>', methods=['GET', 'PUT', 'DELETE'])
 api.add_resource(UserPets, '/users/<string:userId>/pets', methods=['GET', 'POST'])
@@ -24,6 +29,3 @@ api.add_resource(SimilarPets, '/similar-pets', methods=['POST'])
 #TODO: add endpoint to CREATE / DELETE PET PHOTOS
 # api.add_resource(UserPet, '/users/<string:userId>/pets/<string:petId>/photos', methods=['POST'])
 # api.add_resource(UserPet, '/users/<string:userId>/pets/<string:petId>/photos/:photoId', methods=['DELETE'])
-
-if __name__ == '__main__':
-    app.run()
