4e4fd2dc034b16b6780d743f876fe1266f83897d
Maria Eugenia Mariotti <eugeniamariotti93@gmail.com>
2022-08-09 02:49:35 -0300

    Working version of pet transfer

--- src/main/app.py
+++ src/main/app.py
@@ -5,7 +5,8 @@ from flask_restful import Api
 from src.main.resources.fosterVolunteerProfile import FosterVolunteerProfile, FosterVolunteerProfiles
 from src.main.resources.notice import Notices, UserNotices, UserNotice, Notice
 from src.main.resources.pet import UserPet, UserPets
-from src.main.resources.petsFosterHistory import PetFosterHistory, PetFosterHistoryEntry, PetTransference
+from src.main.resources.petsFosterHistory import PetFosterHistory, PetFosterHistoryEntry
+from src.main.resources.petTransfer import PetTransfer, PetTransferCancellation
 from src.main.resources.user import User, Users, UserPwd, UserPwdReset, UserContactInfo
 from src.main.resources.photo import Photo, UserProfilePicture
 from src.main.resources.ping import Ping
@@ -56,7 +57,8 @@ api.add_resource(FosterVolunteerProfiles, '/fosterVolunteerProfiles', methods=['
 
 api.add_resource(PetFosterHistoryEntry, '/pets/<string:petId>/fosterHistory/<string:historyId>', methods=['GET', 'PUT', 'DELETE'])
 api.add_resource(PetFosterHistory, '/pets/<string:petId>/fosterHistory', methods=['GET', 'POST'])
-api.add_resource(PetTransference, '/pets/<string:petId>/transfer', methods=['GET', 'POST'])
+api.add_resource(PetTransfer, '/pets/<string:petId>/transfer', methods=['GET', 'POST'])
+api.add_resource(PetTransferCancellation, '/pets/<string:petId>/transfer/<string:transferId>/cancel', methods=['POST'])
 
 api.add_resource(FacebookPostProcessor, '/facebook', methods=['GET'])
 
